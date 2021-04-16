43d75c38981a59b4ae9df8b4f9558b2dd4e54b3c
Dinis <dinis@lage.pw>
2021-04-16 15:42:35 +0100

    Restructure base classes and the way the classes are configured. Add fixtures and testing with the requests.

--- Gemfile
+++ Gemfile
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in fantasticstay_api.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
+gem 'dotenv', '~> 2.7'
 
-gem "rspec", "~> 3.0"
-
-gem "rubocop", "~> 0.80"
+group :test do
+  gem 'rake', '~> 13.0'
+  gem 'rspec', '~> 3.0'
+  gem 'rubocop', '~> 0.80'
+  gem 'vcr', '~> 6.0'
+  gem 'webmock', '~> 3.12'
+end
--- lib/fantasticstay_api/client.rb
+++ lib/fantasticstay_api/client.rb
@@ -1,62 +1,35 @@
 # frozen_string_literal: true
 
+require 'logger'
+require_relative 'api'
+
 module FantasticstayApi
   # Main client class that implements communication with the API
-  class Client
-    include FantasticstayApi::HttpStatusCodes
-    include FantasticstayApi::ApiExceptions
-
-    API_ENDPOINT = "https://api.fsapp.io"
-    HTTP_STATUS_MAPPING = {
-      HTTP_BAD_REQUEST_CODE => BadRequestError,
-      HTTP_UNAUTHORIZED_CODE => UnauthorizedError,
-      HTTP_FORBIDDEN_CODE => ForbiddenError,
-      HTTP_NOT_FOUND_CODE => NotFoundError,
-      HTTP_UNPROCESSABLE_ENTITY_CODE => UnprocessableEntityError,
-      "default" => ApiError
-    }.freeze
-    attr_reader :api_token
-
-    def initialize(api_token = nil)
-      @api_token = api_token
-    end
-
+  class Client < API
     def listings
-      request(
+      response = request(
         http_method: :get,
-        endpoint: "listings"
+        endpoint: 'listings'
       )
-    end
-
-    private
-
-    def client
-      @client ||= Faraday.new(API_ENDPOINT) do |client|
-        client.request :url_encoded
-        client.adapter Faraday.default_adapter
-        client.headers["x-api-key"] = @api_token
+      listings = response['listings']
+      listings.each do |l|
+        l.transform_keys!(&:to_sym)
       end
+      total = response['total']
+      { listings: listings, total: total }
     end
 
-    def request(http_method:, endpoint:, params: {})
-      response = client.public_send(http_method, endpoint, params)
-      parsed_response = Oj.load(response.body)
-
-      return parsed_response if response_successful?(response)
-
-      raise error_class(response), "Code: #{response.status}, response: #{response.body}"
-    end
-
-    def error_class(response)
-      if HTTP_STATUS_MAPPING.include?(response.status)
-        HTTP_STATUS_MAPPING[response.status]
-      else
-        HTTP_STATUS_MAPPING["default"]
+    def integrations
+      response = request(
+        http_method: :get,
+        endpoint: 'integrations'
+      )
+      integrations = response['integrations']
+      integrations.each do |i|
+        i.transform_keys!(&:to_sym)
       end
-    end
-
-    def response_successful?(response)
-      response.status == HTTP_OK_CODE
+      total = response['total']
+      { integrations: integrations, total: total }
     end
   end
 end
